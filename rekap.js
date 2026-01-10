bdbcffb8176670390666c52afa799ba9ce49d249
caemraul-creator <caemraul@gmail.com>
2026-01-10 09:20:40 +0700

    rekap hideen

--- rekap.js
+++ rekap.js
@@ -1,7 +1,7 @@
 // ======================
 // CONFIG & STATE
 // ======================
-const REKAP_HIDDEN_COLUMNS = ['CreatedAt', 'ApprovedBy', 'ApprovedDate', 'DoneBy', 'DoneDate'];
+const REKAP_HIDDEN_COLUMNS = ['CreatedAt', 'ApprovedBy', 'ApprovedDate', 'DoneBy', 'DoneDate', 'Rejectedby'];
 const NUMBER_COLUMNS = ['Qty'];
 const CURRENCY_COLUMNS = ['Price', 'Nominal'];
 const DATE_COLUMNS = ['SubmissionDate', 'LastBuyingDate', 'OrderDate'];
