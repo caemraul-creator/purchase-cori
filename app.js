70842e967dd4d9c329cdb406a8a8e4fc89bd4d97
caemraul-creator <caemraul@gmail.com>
2026-06-23 22:32:34 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ window.APP_VERSION = 'v4.4.5';
 // 1. CONFIG
 // =====================================================
 
-const API_URL = "https://script.google.com/macros/s/AKfycbxE7RqF61KbU1AB1YV0Ck4zPJSBJdVDbAYchhmEoHYweoczzlZBPESKYGiqfDTKnCxzDw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbw3lWUjVJTMwN6rToovwtcUx0OXaeWlRtR7RRjPBJfV2Ay5_xXzUyP449FI-7-MCUfx9w/exec";
 
 const FIREBASE_CONFIG = {
   apiKey: "AIzaSyBY6B_AMQjeWCfzQiVPtQCLTlTz3ShInwo",
@@ -1446,7 +1446,7 @@ window.ROLE_NAMES = ROLE_NAMES;
           cell = '<td class="text-center"><span class="status ' + status + '">' + safeV + '</span></td>';
         }
 
-        if (h === 'ID') {
+        if (h === 'ID' && hasActions) {
           var safeId = _escapeHtml(r.ID);
           if (page === 'approval') {
             cell += '<td class="text-center" style="white-space:nowrap;">' +
